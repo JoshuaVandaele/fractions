729d1f8dc417ae489adf004b6e9558b6f67faf69
Joshua Vandaële <joshua@vandaele.software>
2024-08-01 07:10:18 +0200

    feat: Show progress in pi example

--- examples/pi.rs
+++ examples/pi.rs
@@ -5,10 +5,11 @@ fn main() {
     let mut pi = Fraction::new(3, 1, FractionSign::Positive);
     let mut sign = FractionSign::Positive;
 
-    for i in (2..20).step_by(2) {
+    for i in (2..22).step_by(2) {
         let term = Fraction::new(4, i * (i + 1) * (i + 2), sign);
         pi += term;
         sign = !sign;
+        println!("{}: {pi} - {}", i / 2, pi.to_decimal_string(16));
     }
 
     println!(
